1c48bf15f147bcac53b6982ebeba8d3406c763fd
Paolo Basso <paolo.basso87@gmail.com>
2017-09-16 16:31:01 +0200

    Avanzamento, refactoring

--- static/js/localization.js
+++ static/js/localization.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
     $('#i18_navbar').i18n();
     $('#i18_about').i18n();
     $('#i18_skills').i18n();
-    $('#i18_projects').i18n();
-    $('#i18_career').i18n();
+    $('#i18_universities').i18n();
+    $('#i18_campaigns').i18n();
     $('#i18_blog').i18n();
     $('#i18_link').i18n();
   };
